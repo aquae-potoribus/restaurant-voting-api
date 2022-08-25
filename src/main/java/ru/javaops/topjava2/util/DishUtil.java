a22e36d1566d1adcc71a35a8ce8b1105909a9c90
snejochek <sergeikondrashkins@gmail.com>
2022-08-25 15:07:33 +0300

    .

---
+++ src/main/java/ru/javaops/topjava2/util/DishUtil.java
@@ -0,0 +1,22 @@
+package ru.javaops.topjava2.util;
+
+import lombok.experimental.UtilityClass;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import ru.javaops.topjava2.model.Dish;
+import ru.javaops.topjava2.model.Menu;
+import ru.javaops.topjava2.model.Role;
+import ru.javaops.topjava2.model.User;
+import ru.javaops.topjava2.to.DishTo;
+import ru.javaops.topjava2.to.UserTo;
+
+@UtilityClass
+public class DishUtil {
+
+    public static final PasswordEncoder PASSWORD_ENCODER = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+
+    public static Dish updateFromTo(DishTo dishTo, Menu menu) {
+        return new Dish(dishTo.getName(), dishTo.getPrice(), menu, dishTo.getId());
+    }
+
+}
